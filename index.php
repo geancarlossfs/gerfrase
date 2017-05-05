dd43c8f09fe49370077466760986f6e69164c16e
geancarlossfs <torinoge@gmail.com>
2017-05-05 09:28:26 -0300

    upload dos arquivos iniciais da aplicação

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require'logica.php'; ?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <title>Document</title>
+    <style>
+       body {
+           background-color: burlywood;
+       }
+        h2 {color: blueviolet;
+        }
+    </style>
+</head>
+<body>
+
+    <h2>< <?= "Olá $tratamento $nome, $qualidade $lugar "?></h2>
+
+</body>
+</html>
---
+++ logica.php
@@ -0,0 +1,12 @@
+<?php
+
+$tratamentos = ['Dom ', 'Cavalcante ', 'Macchupichu '];
+$qualidades  = ['o otarrio ', 'o monstro ', 'iluminati '];
+$lugares     = ['do estreito ', 'de araraquara ', 'de leviosa '];
+$nome        = "Josu ";
+
+$tratamento = $tratamentos[rand(0, 2)];
+$qualidade  = $qualidades[rand(0, 2)];
+$lugar      = $lugares[rand(0, 2)];
+
+?>
